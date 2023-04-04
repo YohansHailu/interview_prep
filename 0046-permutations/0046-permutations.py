6dc054480e3204daf714577bc253801d642ba0a1
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2023-04-04 13:27:39 +0300

    Time: 42 ms (58.87%), Space: 14 MB (76.73%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def helper(self, nums, cur_perm):
+        if len(nums) == 0:
+            self.all_perms.append(tuple(cur_perm))
+        
+        for i in range(len(nums)):
+            cur_perm.append(nums[i])
+            self.helper(nums[:i]+nums[i+1:],cur_perm)
+            cur_perm.pop()
+        
+        
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.all_perms = []
+        self.helper(nums, [])
+        return self.all_perms
+        
