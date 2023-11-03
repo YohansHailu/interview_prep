d989fbe526766883f601c0349e4d622778f89cd9
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2023-11-03 09:36:31 +0300

    Create nothing.py

---
+++ nothing.py
@@ -0,0 +1,11 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param board: the given board
+    @return: nothing
+    """
+    def game_of_life(self, board: List[List[int]]):
+        # Write your code here
